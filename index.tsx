5908f4b4cef85bab7e96746b143300cef55ecfcd
mina-ima <h.imanaka@mdk-j.com>
2025-12-08 13:52:11 +0900

    refactor: Integrate Tailwind CSS into build process

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import './polyfills'; // Must be the very first import
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
